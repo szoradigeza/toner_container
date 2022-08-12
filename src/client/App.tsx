3328d0c3f76a5e9af37c467e640d9847c372e0af
Geza Szoradi <geza.szilard@vodafone.com>
2022-08-12 09:49:44 +0200

    Commit test

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -14,6 +14,7 @@ import { userState } from './stores/user';
 const history = createBrowserHistory();
 
 const App: React.FC = () => {
+    console.log('asd');
     const [user, setUser] = useRecoilState(userState);
     return (
         <BrowserRouter>
